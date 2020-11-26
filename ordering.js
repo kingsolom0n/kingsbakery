b23fca7906b9b2112a871e9424819a0edcd983c3
Solomon Otoo <suprman2020@penguin>
2020-11-26 00:46:30 -0500

    Adding arrays

---
+++ ordering.js
@@ -0,0 +1,3 @@
+let dessert = ["Red Velvet Cupcakes (dozen)", "Southern Butter Pound Cake", "Cream Cheese Muffins (dozen)", "Bailey's & Chocolate Chip Cheesecake" ];
+let deliveryDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+let deliveryOption = ["In-Store Pickup", "Curbside Pickup", "Home Delivery"];
